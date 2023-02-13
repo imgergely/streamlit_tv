7fe2dd7bdc57a58a268d36ffc1ee545418ffd2de
imgergely <69424876+imgergely@users.noreply.github.com>
2023-02-13 17:12:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,37 @@
+
+
 import streamlit as st
 import streamlit.components.v1 as components
 
 
+def run(url):
+    components.html(
+    """
+    <video controls autoplay="true" id="video">
+    <source 
+        src={}
+        type="video/mp4" />
+    </video>
+    """.format(url),
+    height=1754,
+    width=1024)
+
+
 def app():
-    
-    col1, col2 = st.columns(2)
+    col1, col2= st.columns([0.3 ,1])
     with col1:
         tv2 = st.button("TV2")
     with col2:
         rtl = st.button("RTL")
 
     if tv2:
-        components.html(
-        """
-        <video controls autoplay="true" id="video">
-        <source 
-            src="https://cdn.mediaklikk.org:443/tv2/0MjMxEjMxITM" 
-            type="video/mp4" />
-        </video>
-        """,
-        height=1754,
-        width=1240)
-        
+        run("https://cdn.mediaklikk.org:443/tv2/0MjMxEjMxITM")
+       
+
     if rtl:
-        components.html(
-        """
-        <video controls autoplay="true" id="video">
-        <source 
-            src="https://cdn.mediaklikk.org:443/rtl/0MjMxEjMxITM" 
-            type="video/mp4" />
-        </video>
-        """,
-        height=1754,
-        width=1240)
+        run("https://cdn.mediaklikk.org:443/rtl/0MjMxEjMxITM")
+       
+
 
 if __name__ == "__main__":
     app()
-
